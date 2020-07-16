079325834d56c4cbb04099cb059526b8c63512d0
Keyser Soze <christopherjgaines@gmail.com>
2020-07-15 22:43:40 -0400

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=chrisgaines/machine-learning-api
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run machine-learning-api --image=$dockerpath --port=80
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods 
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward machine-learning-api-55fdcf989b-qjljl 8000:80
